9b4ef3ab363466d5c6264fc3a818010520826d43
GauravChinavle <chinawalegaurav@gmail.com>
2021-05-20 00:58:00 +0530

    cors issue v3

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export default function App() {
       try{
         const urlState = 'https://cdn-api.co-vin.in/api/v2/admin/location/states';
         const response = await fetch(urlState,{
-          mode: 'cors',
+          mode: 'no-cors',
           headers: {
           "Content-Type": "application/json",
           'Access-Control-Allow-Origin':'*',
--- src/GetList.js
+++ src/GetList.js
@@ -78,7 +78,7 @@ function GetDistrict(props) {
       try{
         const urlDist = `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state}`;
         const response = await fetch(urlDist,{
-          mode: 'cors',
+          mode: 'no-cors',
           headers: {
           "Content-Type": "application/json",
           'Access-Control-Allow-Origin':'*',
@@ -123,70 +123,3 @@ const handleChangeDistrict = (event) => {
     </>
   );
 }
-
-// export default function GetList(props) {
-//   const classes = useStyles();
-//   const [state, setState] = React.useState("");
-//   const [district, setDistrict] = React.useState();
-
-//   var [distList,setDistList]= useState([]);
-  
-//   const {stateList} = props;
-//   const handleChangeState = (event) => {
-//     setState(event.target.value);
-//     const id = event.target.value;
-//     async function fetchDistList(){
-//     try{
-//       const urlDist = `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`;
-//       const response = await fetch(urlDist);
-//       const resJSON = await response.json();
-//       const districts = await resJSON.districts;
-//       console.log("hello");
-//       setDistList(districts);
-//     } catch {
-
-//   }
-// }fetchDistList();
-// };
-
-// const handleChangeDistrict = (event) => {
-//   setDistrict(event.target.value);
- 
-// };
-  
-//   return (
-//     <>
-//       <FormControl variant="outlined" className={classes.formControl}>
-//         <InputLabel id="demo-simple-select-outlined-label">State</InputLabel>
-//         <Select
-//           labelId="demo-simple-select-outlined-label"
-//           id="demo-simple-select-outlined"
-//           value={state}
-//           onChange={handleChangeState}
-//           label="state"
-//         >{
-//             stateList.map(item=>(
-//                 <MenuItem value={item.state_id}>{item.state_name}</MenuItem>
-            
-//             ))}
-//         </Select>
-//       </FormControl>
-//       <FormControl variant="outlined" className={classes.formControl}>
-//         <InputLabel id="demo-simple-select-outlined-label">District</InputLabel>
-//         <Select
-//           labelId="demo-simple-select-outlined-label"
-//           id="demo-simple-select-outlined"
-//           value={district}
-//           onChange={handleChangeDistrict}
-//           label="District"
-//         >
-//           {
-//             distList.map(item=>(
-//                 <MenuItem value={item.district_id}>{item.district_name}</MenuItem>
-//             ))}
-//         </Select>
-//       </FormControl>
-
-//     </>
-//   );
-// }
